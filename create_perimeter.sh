720dc9b753bd348e00c45b72dbeadb6351e8d85d
migo <amegahed94@gmail.com>
2021-03-22 18:53:00 -0400

    in progress

---
+++ create_perimeter.sh
@@ -0,0 +1,14 @@
+# Lists organization (that you have access to) and their IDs
+gcloud organizations list
+
+# Returns the POLICY_NAME for amega.info == 212996153568
+gcloud access-context-manager policies list \
+    --organization 422800608572
+
+# Creating a perimeter with ingress policies
+gcloud access-context-manager perimeters create my_access_level \
+  --title="testing ingress policies" \
+  --resources=projects/812330920236 \
+  --restricted-services=bigquery.googleapis.com \
+  --policy=212996153568 \
+  --set-ingress-policies=ingress_policies.yaml
