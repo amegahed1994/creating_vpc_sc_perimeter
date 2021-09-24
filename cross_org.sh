49cb33bf886163c64ff89d790b0ee79433aeccf9
megaman <amegahed94@gmail.com>
2021-09-24 11:32:17 -0400

    Update cross_org.sh

--- cross_org.sh
+++ cross_org.sh
@@ -1,1 +1,11 @@
-gcloud beta access-context-manager perimeters update PERIMETER --policy=POLICY --set-ingress-policies=YAML_FILE
+# Lists organization (that you have access to) and their IDs
+gcloud organizations list
+
+# Returns the POLICY_NAME for megahed.ca == 518900189187
+gcloud access-context-manager policies list \
+    --organization 518900189187
+    
+gcloud beta access-context-manager perimeters update perimeter \ 
+    --policy=20639544693 --set-ingress-policies=myingress.yaml
+
+
